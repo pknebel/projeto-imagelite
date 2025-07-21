dc5a7aa1544af827bfb08894b5b96285b40dd3e9
Pedro Henrique Knebel <93292374+pknebel@users.noreply.github.com>
2025-07-21 20:41:03 -0300

    initial commit

---
+++ imageliteapi/src/main/java/com/github/pknebel/imageliteapi/ImageliteapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pknebel.imageliteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageliteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageliteapiApplication.class, args);
+	}
+
+}
---
+++ imageliteapi/src/test/java/com/github/pknebel/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pknebel.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
